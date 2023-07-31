80f103028850109ee41065987e6cee32d1505586
marykolison <48145229+marykolison@users.noreply.github.com>
2023-07-31 14:54:22 -0400

    Update model.py

--- database/model.py
+++ database/model.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'database'  # Replace with your database URL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'db'
 db = SQLAlchemy(app)
 
 class Cake(db.model):
