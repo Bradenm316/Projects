23db11600e49d66ae04684a46fcc16d88702f014
marykolison <48145229+marykolison@users.noreply.github.com>
2023-07-31 13:57:42 -0400

    Update model.py

--- database/model.py
+++ database/model.py
@@ -46,23 +46,6 @@ class Order(db.model):
     status = db.Column(db.String(64), nullable=False)
     cancel_fee = db.Column(db.Float)
 
-class User(db.model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64), nullable=False, unique=True)
-    password_hash = db.Column(db.String(128))
-    role = db.Column(db.String(64), nullable=False)
-
-    @property
-    def password(self):
-        raise AttributeError('password is not a readable attribute')
-
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
 
 if __name__ == '__main__':
     app.run()
