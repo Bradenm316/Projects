4511f20b1cf31516120b1b5bb5ec7998b8812f3d
eekim2003 <134553543+eekim2003@users.noreply.github.com>
2023-08-06 21:32:49 -0400

    Update app.py eric

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ owners =[]
 order = {}
 customer_info ={}
 
-@app.route('/')f
+@app.route('/')
 def index_page():
     """
     Renders the index page when the user is at the `/` endpoint, passing along default flask variables.
